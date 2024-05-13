f44289f958da420dafc085d352c875f1d4ad6a92
kingaer04 <danielanifowoshe04@gmail.com>
2024-05-13 22:42:08 +0100

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,6 +1,26 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const app = express()
+// mongoose.connect(
+//     "mongodb://localhost:27017/EccaLagos",
+//     {useNewUrlParser:true}
+// )
+
+// const db = mongoose.connection
+// db.once("open", ()=>{
+//     console.log("Connected Successfully") // return a message to the terminal
+// })
+
+mongoose.connect(process.env.MONGO)
+
+const db = mongoose.connection
+db.once("open", () => {
+    console.log("Database connected succesfully!")
+})
 
 app.set('port', process.env.PORT || 3000)
 
